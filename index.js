bdea9b8dffa3abeae7368ad98b3e552347b33265
Zendard <thijsmuylaert@hotmail.com>
2023-06-25 21:22:36 +0200

    Add game function

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
 	const choice = Math.floor(Math.random * 3);
 	return choice;
@@ -17,11 +20,21 @@ function playRound(playerSelection, computerSelection) {
 function getPlayerChoice() {
 	const choice = prompt("Rock, Paper or Scissors?")
 	switch (choice.toLowerCase()) {
-		case rock: { return 1 }
-		case paper: { return 2 }
-		case scissors: { return 3 }
+		case rock: { return 1; }
+		case paper: { return 2; }
+		case scissors: { return 3; }
 	}
 }
 function game() {
-
+	for (let i = 0; i++) {
+		let playerChoice = getPlayerChoice();
+		let computerChoice = getComputerChoice();
+		let win = playRound(playerChoice, computerChoice);
+		switch (win) {
+			case null: { playerScore++; computerScore++; break }
+			case true: { playerScore++; break }
+			case false: { computerScore++; break }
+		}
+		console.log(`Player score:${playerScore}\nComputer score:${computerScore}`)
+	}
 }
